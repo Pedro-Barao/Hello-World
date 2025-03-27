e051471bde597cc99cc81b4183cb8dbda56de1a8
Pedro Barão <predrobarao183@gmail.com>
2025-03-27 14:49:04 -0300

    Add files via upload

--- Exerc(V2)6.c
+++ Exerc(V2)6.c
@@ -6,9 +6,7 @@
 
 */
 
-
 #include <stdio.h>
-
 
 int main()
 {
@@ -17,10 +15,7 @@ int main()
     int horas_trabalhadas;
     float valor_hora_trabalhada;
     int dependentes;
-    int idade_dependente1;
-    int idade_dependente2;
-    int idade_dependente3;
-
+    int menores14;
 
     printf("\nBem-vindo(a) a calculadora salarial\n");
     printf("Quantas horas voce trabalha por mes?: ");
@@ -30,72 +25,21 @@ int main()
     printf("Quantos dependetes voce tem?: ");
     scanf("%d", &dependentes);
 
-    if (dependentes == 0){
+    if (dependentes > 0)
+    {
+        printf("\nVoce possui %d depenedentes \n", dependentes);
+        printf("Quantos dependentes sao menores que 14 anos?...");
+        scanf("%d", &menores14);
+        salario_bruto = (menores14*13.48)+(horas_trabalhadas*valor_hora_trabalhada);
+
+    }
+    else
+    {
         printf("\nVoce nao possui depenedentes\n");
-
         salario_bruto = horas_trabalhadas * valor_hora_trabalhada;
-
-    } else if (dependentes == 1){
-        printf("\nVoce possui 1 dependente\n");
-        printf("Quantos anos tem seu dependente?:");
-        scanf("%d", &idade_dependente1);
-
-        if (idade_dependente1 >= 13){
-            
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (dependentes * 13.48);
-
-        } else {
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada;
-        }
-
-    } else if (dependentes == 2){
-        printf("\nVoce possui 2 dependentes\n");
-        printf("Quantos anos tem seu primeiro dependente?:");
-        scanf("%d", &idade_dependente1);
-        printf("Quantos anos tem seu segundo dependente?:");
-        scanf("%d", &idade_dependente2);
-
-        if (idade_dependente1 && idade_dependente2 <= 13){
-            
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (dependentes * 13.48);
-
-        } else if (idade_dependente1 || idade_dependente2 <= 13){
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (1 * 13.48);
-        } else{
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada;
-        }
-    } else if (dependentes == 3){
-        printf("\nVoce possui 3 dependentes\n");
-        printf("Quantos anos tem seu primeiro dependente?:");
-        scanf("%d", &idade_dependente1);
-        printf("Quantos anos tem seu segundo dependente?:");
-        scanf("%d", &idade_dependente2);
-        printf("Quantos anos tem seu terceiro dependente?:");
-        scanf("%d", &idade_dependente3);
-
-        if (idade_dependente1 && idade_dependente2 && idade_dependente3 <= 13){
-            
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (dependentes * 13.48);
-
-        } else if (idade_dependente1 && idade_dependente2 <= 13 || idade_dependente1 && idade_dependente3 <= 13 || idade_dependente2 && idade_dependente3 <= 13){
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (2 * 13.48);
-
-        } else if (idade_dependente1 || idade_dependente2 || idade_dependente3 <= 13){
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (1 * 13.48);
-        } else{
-
-            salario_bruto = horas_trabalhadas * valor_hora_trabalhada + (dependentes * 13.48);
-
-        }
     }
 
-
-    printf("\nSeu salario bruto e de %.2f", salario_bruto);
+    printf("\n============================================\n");
+    printf("Seu salario bruto e de.....%.2f", salario_bruto);
     return 0;
-
 }
