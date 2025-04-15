d2d6ae54b9abf2178a7be82f1f3cbe566a2de31a
Pedro Barão <predrobarao183@gmail.com>
2025-04-15 16:25:38 -0300

    Add files via upload

--- CMR(V1)/Exerc(CMR_V1)7.c
+++ CMR(V1)/Exerc(CMR_V1)7.c
@@ -13,18 +13,57 @@ int main()
 {
 
     int i;
-    int idade[5];
+    float altura;
+    float maior_altura=0;
+    float menor_altura=0;
+    int numero;
+    int maior_numero=0;
+    int menor_numero=0;
 
 
     for( i = 0; i < 5; i++)
     {
 
-        printf("digite a idade da pessoa %d: ", i+1);
-        scanf("%d", &idade[i]);
+        printf("\nDigite a altura da pessoa %d: ", i+1);
+        scanf("%f", &altura);
+
+        printf("\nDigite a numero da pessoa %d: ", i+1);
+        scanf("%d", &numero);
+
+        if (i==1)
+        {
+
+            maior_numero = numero;
+            menor_numero = numero;
+            maior_altura = altura;
+            menor_altura = altura;
+
+        }
+
+        else
+        {
+
+            if (altura > maior_altura)
+            {
+
+                maior_altura = altura;
+                maior_numero = numero;
+
+            }
+
+            if (altura < menor_altura)
+            {
+
+                menor_altura = altura;
+                menor_numero = numero;
+
+            }
+        }
 
     }
 
-    printf("\n%d", idade[1]);
+    printf("\nO aluno mais alto tera %.2f de altura com o numero %d", maior_altura, maior_numero);
+    printf("\nO aluno mais baixo tera %.2f de altura com o numero %d", menor_altura, menor_numero);
 
 
     return 0;
