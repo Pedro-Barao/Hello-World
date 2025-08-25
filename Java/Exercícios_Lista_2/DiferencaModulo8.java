1577f18b20736226f7e0aff27545dffc9e6337c5
Pedro Barão <predrobarao183@gmail.com>
2025-08-25 09:47:46 -0300

    Add files via upload

---
+++ Java/Exercícios_Lista_2/DiferencaModulo8.java
@@ -0,0 +1,57 @@
+/*
+
+Autor: Gabriel Nascimento, Guilherme Cortez, Henrique Oliveira e Pedro Barão
+Data: 15/08/2025
+Objetivo: Uma função que entrega a diferença de dois números em módulo
+
+ */
+
+package Exercícios_Lista_2;
+import java.util.Scanner;
+
+public class DiferencaModulo8 {
+    
+    
+    public static int ResultadoModulo(int a, int b)
+    {
+        
+        if (a < 0)
+        {
+            
+            a = (a * (-1));
+            
+        }
+        
+        if (b < 0)
+        {
+            
+            b = (b * (-1));
+            
+        }
+        
+        return a - b;
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        
+        try(Scanner scanner = new Scanner(System.in))
+        {
+            
+            int resultado;
+            
+            System.out.println("\nOla usuario! Insira 2 numeros para relizarmos sua diferenca em modulo: ");
+            int numero1 = scanner.nextInt();
+            
+            System.out.println("\nAgora o segundo numero: ");
+            int numero2 = scanner.nextInt();
+            
+            
+            resultado = ResultadoModulo(numero1, numero2);
+            
+            System.out.println("\nO resultado da diferenca entre " + (numero1 * (-1)) + " e " + (numero2 * (-1)) + " e de = " + resultado);
+            
+        }
+    }
+}
---
+++ Java/Exercícios_Lista_2/EntreUmeCem7.java
@@ -0,0 +1,58 @@
+/*
+
+Autor: Gabriel Nascimento, Guilherme Cortez, Henrique Oliveira e Pedro Barão
+Data: 15/08/2025
+Objetivo: Uma função que verifica se um número entre 1 e 100
+
+ */
+
+package Exercícios_Lista_2;
+import java.util.Scanner;
+
+public class EntreUmeCem7 {
+    
+    
+    public static boolean UmaCem(int a)
+    {
+        
+        boolean verificador;
+        
+        verificador = a <= 100 && a >= 1;
+        
+        return verificador;
+        
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            
+            boolean verificador;
+            
+            
+            System.out.println("\nOla usuario! Digite um numero e direi se ele esta entre 1 e 100: ");
+            int numero = scanner.nextInt();
+            
+            
+            verificador = UmaCem(numero);
+            
+            
+            if (verificador == true)
+            {
+                
+                System.out.println("\nO numero " + numero + " esta entre 1 e 100");
+                
+            }
+            
+            else
+            {
+                
+                System.out.println("\nO numero " + numero + " nao esta entre 1 e 100");
+                
+            }
+        }
+    }
+}
