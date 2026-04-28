bfd8c83490180ad160128834ca54bf613119de27
Pedro Barão <predrobarao183@gmail.com>
2026-04-28 17:17:38 -0300

    Add files via upload

---
+++ HTML/Tarefa_2/Exemplo_JavaScript/script.js
@@ -0,0 +1,75 @@
+//Printa no console
+console.log("Hello World");
+
+
+//Criar variáveis
+const numero = 3;
+console.log(numero);
+
+const texto = "Boa Noite";
+console.log(texto);
+
+/*Constante não muda o valor
+
+numero = 2;
+console.log(numero);
+
+*/
+
+
+//Ultrapassado
+var numero1 = 2;
+console.log(numero1);
+
+numero1 = 3;
+console.log(numero1);
+
+
+//Novo
+let numero2 = 2;
+console.log(numero2);
+
+numero2 = 3;
+console.log(numero2);
+
+
+//Arrays
+let vetor = [1, 2, 3, "dois"];
+
+console.log(vetor);
+
+vetor = false;
+
+console.log(vetor);
+
+const map = {"chave1": 1, "chave2": 2, "chave3" : 3};
+
+console.log(map);
+
+
+//Null
+const nulo = null;
+console.log(nulo);
+
+
+//Condicionais
+if(1 == 1)
+{
+
+    console.log("oi");
+
+}
+
+if(numero1 == 4)
+{
+
+    console.log("Ta Certo!");
+
+}
+
+else
+{
+
+    console.log("Ta Errado");
+    
+}
